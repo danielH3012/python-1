413c46614d7e92c329a2d9177d8145d2f8879f8a
danielH3012 <97351275+danielH3012@users.noreply.github.com>
2023-10-06 06:00:27 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+print("halo",end="$")
+print("apa kabar",end=" ")
+print("anda semua")
+print("")
+
+a = "selamat pagi"
+print(a,"\n")
+b = "selamat siang"
+print(b)
+print("\n")
+c="selamat malam"
+print(c,"\n")
+
+x="no"
+y="nama"
+z="nim"
+print(x,"\t",y,"\t\t",z)
