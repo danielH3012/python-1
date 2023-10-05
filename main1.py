c08facd11cc64a307f3b2a45a679076f7adbf3af
danielH3012 <97351275+danielH3012@users.noreply.github.com>
2023-10-06 06:01:11 +0700

    Create main1.py

---
+++ main1.py
@@ -0,0 +1,16 @@
+nim = "32230127"
+nama = "daniel herdjanto"
+prodi="informatika"
+
+print(nim)
+print(nama)
+print(prodi)
+
+print(nim,"\n")
+print(nama,end=" ")
+print(prodi)
+
+print(nim,end=" ")
+print(nama)
+print("\n")
+print(prodi)
